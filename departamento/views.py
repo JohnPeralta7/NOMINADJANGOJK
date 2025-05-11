07918daede098177051f734ff173b0a5f3efc5f0
JohnPeralta7 <jperaltar3@unemi.edu.ec>
2025-05-11 00:22:24 -0500

    Create implementado - corregir valid

--- departamento/views.py
+++ departamento/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Departamentos
+from .forms import DepartamentoForm
 
 # Create your views here.
 def listado(request):
-    query = request.GET.get('q')
-    departamentos = Departamentos.objects.all()
+    query = request.GET.get('q') # None
+    departamentos = Departamentos.objects.all() #[{'id' : 1, 'description' : 'TICS'}, {'id' : 2, 'description' : 'Inspector'}]
     if query:
         departamentos = Departamentos.objects.filter(description__icontains=query)
     return render(request, 'departamento/listado.html', {
@@ -13,7 +14,19 @@ def listado(request):
     })
 
 def create(request):
-    ...
+    context = {'title': 'Ingresar Departamento'}
+    if request.method == 'GET':
+        form = DepartamentoForm()
+        context['form'] = form #Creo una llave form y tomara de valor mi objeto formulario de departamento
+        return render(request, 'departamento/create.html', context ) #{'title': 'Ingresar Departamento', 'form' : form}
+    else:
+        form = DepartamentoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('departamento:listado')
+        else:
+            context['form'] = form
+            return render(request, 'departamento/create.html')
 
 def update(request, id):
     ...
