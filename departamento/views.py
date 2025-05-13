6cb013b6c489b0e805f617ac91315934d9332aca
JohnPeralta7 <jperaltar3@unemi.edu.ec>
2025-05-13 00:28:47 -0500

    update-delete implementado

--- departamento/views.py
+++ departamento/views.py
@@ -29,7 +29,30 @@ def create(request):
             return render(request, 'departamento/create.html')
 
 def update(request, id):
-    ...
+    context = {'title' : 'Actualizar Departamento'}
+    departamento = Departamentos.objects.get(pk=id)
+    if request.method == 'GET':
+        form = DepartamentoForm(instance=departamento)
+        context['form'] = form
+        return render(request, 'departamento/update.html', context)
+    else:
+        form = DepartamentoForm(request.POST, instance=departamento)
+        context['form'] = form
+        if form.is_valid():
+            form.save()
+            return redirect('departamento:listado')
+        #tengo que implementarle lo que hara sino funciona
 
 def delete(request, id):
-    ...
+    departamento=None
+    try:
+        departamento = Departamentos.objects.get(pk=id)
+        if request.method == "GET":
+            context = {'title':'Eliminar :','departamento':departamento,'error':''}
+            return render(request, 'departamento/delete.html',context)  
+        else: 
+            departamento.delete()
+            return redirect('departamento:listado')
+    except:
+        context = {'title':'Departamento info','departamento':departamento,'error':'Error al eliminar departamento'}
+        return render(request, 'departamento/delete.html',context)
