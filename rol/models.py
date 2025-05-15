0a54911d393dde4774f01d543f2310de02a2cd98
kfrancor <karenfr397@gmail.com>
2025-05-15 00:37:46 -0500

    logincomplete

--- rol/models.py
+++ rol/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from empleado.models import Empleado
 
 # Create your models here.
+
+class Rol(models.Model): 
+    id_empleado= models.ForeignKey(Empleado, on_delete=models.CASCADE) 
+    aniomes = models.DateField()#202501 
+    sueldo = models.DecimalField(max_digits=10, decimal_places=2) 
+    horas_extra = models.DecimalField(max_digits=10, decimal_places=2) 
+    bono = models.DecimalField(max_digits=10, decimal_places=2) 
+    iess = models.DecimalField(max_digits=10, decimal_places=2) 
+    tot_ing = models.DecimalField(max_digits=10, decimal_places=2) 
+    tot_des = models.DecimalField(max_digits=10, decimal_places=2) 
+    neto = models.DecimalField(max_digits=10, decimal_places=2)
--- tcontrato/views.py
+++ tcontrato/views.py
@@ -1,9 +1,53 @@
 from django.shortcuts import render, redirect
 from .models import TipoContrato
 from .forms import TipoContratoForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
+from django.db import IntegrityError
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+def signup(request):
+    if request.method == 'GET': 
+        return render(request, 'signup.html', {
+            'form' : UserCreationForm
+        })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('t_contrato:listado')
+            except IntegrityError:
+                return render(request, 'signup.html', {
+            'form' : UserCreationForm,
+            'error' : 'Usuario ya existee'
+        })
+        return render(request, 'signup.html', {
+            'form' : UserCreationForm,
+            'error' : 'No coincide la contraseña'
+        })
+            
+def signin(request):
+    if request.method == 'GET':
+        return render(request, 'signin.html', {
+            'form' : AuthenticationForm
+    })
+    else:
+        user = authenticate(request, username=request.POST['username'], password = request.POST['password'])
+        if user is None:
+            return render(request, 'signin.html', {
+            'form' : AuthenticationForm,
+            'error' : 'Usuario o contraseña incorrecta'
+    })
+        else:
+            login(request, user)
+            return redirect('t_contrato:listado')
+
+@login_required
 def tcontratos(request):
     dattcontrato = TipoContrato.objects.all()
     query = request.GET.get('q')
@@ -13,6 +57,7 @@ def tcontratos(request):
         'dattcontrato': dattcontrato, 
         'title': 'Listado de contratos'})
 
+@login_required
 def tcontrato_create(request):
     context = {'title' : 'Ingresar tipo contrato'}
     if request.method == 'GET':
@@ -28,6 +73,7 @@ def tcontrato_create(request):
             context['form'] = form
             return render(request, 'tcontrato/create.html')
 
+@login_required
 def tcontrato_update(request, id):
     context = {'title' : 'Actualizar tcontrato'}
     tcontrato = TipoContrato.objects.get(id=id)
@@ -44,6 +90,7 @@ def tcontrato_update(request, id):
             context['form'] = form
             return render(request, 'tcontrato/update.html')
 
+@login_required
 def tcontrato_delete(request, id):
     from django.core.exceptions import ObjectDoesNotExist
     tcontrato = None
@@ -58,3 +105,9 @@ def tcontrato_delete(request, id):
     except ObjectDoesNotExist:
         context = {'title' : 'Tipo de contrato a Eliminar', 'tcontrato' : tcontrato, 'error' : 'Error al eliminar el tipo de contrato'}
         return render(request, 'tcontrato/delete.html', context)
+    
+     
+@login_required   
+def signout(request):
+    logout(request)
+    return redirect('inicio') 
